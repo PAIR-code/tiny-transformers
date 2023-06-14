88b5d4191a10d900a6e78d042ee83254ffcb4038
Adam Pearce <adampearce@google.com>
2023-06-14 02:06:54 -0400

    animate vocab works

--- decision-boundary/08-animate-vocab-embed/util.js
+++ decision-boundary/08-animate-vocab-embed/util.js
@@ -30,7 +30,10 @@ window.util = (function(){
   }
 
   async function getFile(path, uploadData={}){
-    var [slug, type] = path.replaceAll('..', '').split('.')
+    var [slug, ...type] = path.replaceAll('..', '').split('.')
+    console.log(type)
+    type = _.last(type)
+
 
     var uploadDataStr = JSON.stringify(uploadData)
     slug = path + ' __ ' + uploadDataStr 
