3b67cd28f65a2bd47b9c802b7aa6b963e310060f
Lucas Dixon <ldixon@google.com>
2023-06-20 01:08:54 +0200

    remove commented line

--- animated-transformer/src/lib/holey-prompts/holey_prompts.spec.ts
+++ animated-transformer/src/lib/holey-prompts/holey_prompts.spec.ts
@@ -13,7 +13,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-// gtensor.spec.ts
 import { HoleyPrompt, Hole, RegExpHole, makePrompt } from './holey_prompts';
 
 describe('holey_prompts', () => {
