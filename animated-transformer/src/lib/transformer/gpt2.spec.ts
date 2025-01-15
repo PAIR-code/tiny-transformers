572c3b389712dcd8ce7bac2b788ec27e439eb126
Alicia Fortes Machado <aliciafortesmachado@gmail.com>
2025-01-15 22:54:55 +0100

    Add GPT2 implementation (#50)
    * Started implementation of GPT2.
    
    * Token embeddings are correct, but still need to fix positional encodings and heads.
    
    * Add biases to key, query, value and projection layers.
    
    * Make layer norm work on multi-dimensional parameters.
    
    * Adapt transformer and GPT2 implementations to work with changes in layer norm. Added a test for layer norm init as well.
    
    * Add test for initialization of multi-dimensional layer norm.
    
    * Split init layer norm into two functions since type check was not accepting two different types of outputs out of the init function.
    
    * Updating GPT2 implementation after rebase.
    
    * Make epsilon not trainable and pass it through compute spec in GPT2. Pending to update `transformer_gtensor.ts`.
    
    * Add layer norm outside attention head computation.
    
    * Simplify GPT2 setup.
    
    * Add positional encodings on GPT2. Last step is to clean up the code and tests before creating PR.
    
    * Fixed order of dropouts, layer norms and residual connections.
    
    * Clean-up comments and files, and add support for creating train and eval gpt2 configs.
    
    * Added the casual masking introduced in previous PR. Did not copy the allPastTokensCrossEntropyLoss as it can be reused. Fixed typo in BatchAttnHeadComputation and reusing the same for GPT2.

---
+++ animated-transformer/src/lib/transformer/gpt2.spec.ts
@@ -0,0 +1,138 @@
+/* Copyright 2023 Google LLC. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+import { GTensor } from '../gtensor/gtensor';
+import * as tf from '@tensorflow/tfjs';
+import * as gpt2 from './gpt2';
+import { prepareBasicTaskTokenRep, BasicTaskTokenRep } from '../tokens/token_gemb';
+import * as jstree from '../js_tree/js_tree';
+
+function generateTestTask(): BasicTaskTokenRep {
+  // The vocabulary size for GPT2 is 50257. However, the prepareBasicTaskToken adds four
+  // additional tokens, so we need to set 50257 - 4 dummy tokens for reproducing the vocab
+  // size of GPT2.
+  const tokens = Array(50257 - 4).fill("test");
+  // The BasicTaskTokenRep below is not valid but it's fine since we are just checking the
+  // number of parameters.
+  return prepareBasicTaskTokenRep(tokens);
+}
+
+describe('GTensor Transformers', () => {
+  it('Check number of parameters on GPT2 head', () => {
+    const gpt2Model: gpt2.Config = gpt2.defaultGPT2EvalConfig(generateTestTask(), false);
+    const params = gpt2.initDecoderParams(gpt2Model);
+
+    // Compute number of parameters in the head.
+    const paramCount = jstree.reduce<GTensor<any>, number>(
+      (count, paramObj) => count + paramObj.tensor.size,
+      0,
+      params.layers[0]
+    );
+
+    // Check head size.
+    expect(paramCount).toEqual(7087872);
+  });
+
+  it('Check number of parameters on GPT2', async () => {
+    const gpt2Model: gpt2.Config = gpt2.defaultGPT2EvalConfig(generateTestTask(), false);
+    const params = gpt2.initDecoderParams(gpt2Model);
+
+    // Compute number of parameters in GPT2.
+    const paramCountGPT2 = jstree.reduce<GTensor<any>, number>(
+      (count, paramObj) => count + paramObj.tensor.size,
+      0,
+      params
+    );
+
+    // Check full GPT2 size.
+    expect(paramCountGPT2).toEqual(124439808);
+  });
+
+  it('Test positional embeddings.', async () => {
+    // Set dummy transformer for testing.
+    const embeddingSize = 2;
+    const posEmbeddings = 3;
+    const nHeads = 1;
+    const layerConfig: gpt2.TransformerParamLayerSpec = {
+      nHeads: nHeads,
+      layerNormPreAttention: false,
+      layerNormHeadsProjection: false,
+      addLayerNormBias: false,
+      computeSpec: { residuals: true, dropoutRate: 0, layerNormEpsilon: 1e-5 },
+    };
+    const spec: gpt2.TransformerParamSpec = {
+      inputRep: embeddingSize,
+      kqvRep: embeddingSize / nHeads,
+      layers: Array(nHeads).fill(layerConfig),
+      computeSpec: {
+        dropoutRate: 0.0,
+        layerNormEpsilon: 1e-5
+      },
+      posEncodingSeqLength: posEmbeddings,
+      layerNorm: false,
+      addLayerNormBias: false,
+      addPosEmbeddings: true,
+    };
+    const config: gpt2.Config = {
+      id: 'GPT2Eval',
+      kind: 'Transformer',
+      spec: spec,
+      tokenRep: generateTestTask(),
+      init: {
+        stddev: 0.05, // default
+        mean: 0,
+        seed: 42,
+      },
+    };
+    const params = gpt2.initDecoderParams(config);
+    params.posEmbedding = new GTensor(
+      tf.tensor([
+        [1, 1],
+        [0, 1],
+        [1, 0],
+      ]),
+      ['posId', 'inputRep']
+    );
+    const model = { params, config }
+    const inputExample = new GTensor(
+      tf.tensor([
+        [
+          [1, 2],
+          [3, 4],
+          [5, 6],
+        ],
+        [
+          [1, 2],
+          [3, 4],
+          [5, 6],
+        ],
+      ]),
+      ['batch', 'pos', 'inputRep']
+    );
+    const result = gpt2.addPosEmbeddings(model, inputExample);
+    tf.test_util.expectArraysClose(result.tensor.dataSync(), [
+      [
+        [2, 3],
+        [3, 5],
+        [6, 6],
+      ],
+      [
+        [2, 3],
+        [3, 5],
+        [6, 6],
+      ],
+    ])
+  });
+});
